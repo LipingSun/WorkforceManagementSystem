be1ee6be25a543744bfc7a8f1cabd85e251ddfa2
LipingSun <liping.sun@sjsu.edu>
2015-04-27 12:41:58 -0700

    add login module

--- WorkforceManagementSystem/app.js
+++ WorkforceManagementSystem/app.js
@@ -6,10 +6,14 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
-  , report = require('./routes/reports')
-  , building = require('./routes/building')
   , http = require('http')
   , path = require('path');
+
+var session = require('express-session');
+var report = require('./routes/reports');
+var building = require('./routes/building');
+var login = require('./routes/login');
+
 
 var app = express();
 
@@ -23,6 +27,12 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'CMPE273',
+  resave: false,
+  saveUninitialized: true//,
+  //cookie: { maxAge: 15 * 60 * 1000 }
+}));
 
 // development only
 if ('development' == app.get('env')) {
@@ -30,10 +40,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/signin', routes.signin);
 app.get('/users', user.list);
 app.use('/reports', report);
 app.use('/buildings', building);
+app.use('/login', login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
