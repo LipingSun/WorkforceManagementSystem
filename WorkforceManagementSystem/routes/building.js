c852d243af9bd40bf0c177d39eeedd847622dc76
LipingSun <liping.sun@sjsu.edu>
2015-04-30 01:11:36 -0700

    achieve building update function and add name property for building

--- WorkforceManagementSystem/routes/building.js
+++ WorkforceManagementSystem/routes/building.js
@@ -66,6 +66,7 @@ building.post('/', function (req, res) {
 
     var sql = squel.insert().into('building')
         .set('client_id', req.body.client_id)
+        .set('name', req.body.name)
         .set('address', req.body.address)
         .set('release_date', req.body.release_date)
         .set('service_fee', req.body.service_fee)
@@ -89,6 +90,29 @@ building.post('/', function (req, res) {
 });
 
 //POST /buildings/{building_id}/update
+building.post('/:id/update', function (req, res) {
+    console.log('\nPOST ' + req.originalUrl);
+    var sql = squel.update().table('building');
+    for (var prop in req.body) {
+        sql.set(prop, req.body[prop]);
+    }
+    sql = sql.where('building_id=' + req.params.id).toString();
+
+    console.log('DB Query: ' + sql);
+    connectionPool.query(sql, function (err, data) {
+        if (!err) {
+            console.log('DB Result: ' + JSON.stringify(data));
+            res.json(200, {"message": "Update success"});
+        } else {
+            console.log('DB ERROR: ' + err.message);
+            res.json(500, {
+                'message': 'Error occurred',
+                'success': false,
+                'status': 500
+            });
+        }
+    });
+});
 
 //DELETE /buildings/{building_id}
 building.del('/:id(\\d+)', function (req, res) {
